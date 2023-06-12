7ff6114263c2683ad99414f79581e9ec200b7a52
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-12 23:18:33 +0300

    Update ContactListItem.jsx

--- src/components/ContactListItem/ContactListItem.jsx
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -1,2 +1,23 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import { Item, Text, Marker } from './ContactListItem.styled';
+
+export const ContactListItem = ({ id, name, number }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <Item>
+      <Marker></Marker>
+      <Text>
+        {name}: <span>{number}</span>
+      </Text>
+      <button type="button">Delete</button>
+    </Item>
+  );
+};
+
+ContactListItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+};
