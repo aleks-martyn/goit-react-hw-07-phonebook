b7a9c404b5a4f1a0cedee744716ea90370bbfec9
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-13 23:55:27 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,1 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { fetchContacts, addContact, deleteContact } from './operations';
+
+const initialState = {
+  items: [],
+  isLoading: false,
+  error: null,
+};
