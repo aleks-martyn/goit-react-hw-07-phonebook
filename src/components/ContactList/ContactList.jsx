384e81caf7035d118d6084b680c07f1720e10b9f
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-06-14 14:58:41 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,3 +1,4 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { ContactListItem } from 'components/ContactListItem';
 import { List } from './ContactList.styled';
